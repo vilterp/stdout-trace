bf4b05af0a15a2f83692306f08204acd6b30cc44
Pete Vilter <pete.vilter@gmail.com>
2019-11-24 18:08:49 -0500

    accumulate events in state

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,103 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div>Bloop</div>;
+const wsAddr =
+  process.env.NODE_ENV === "development"
+    ? "localhost:8888"
+    : window.location.host;
+const wsURL = `ws://${wsAddr}/ws`;
+
+type TraceEvent =
+  | {
+      evt: "start_span";
+      id: number;
+      parent_id: number;
+      ts: string;
+      op: string;
+    }
+  | {
+      evt: "log";
+      id: number;
+      ts: string;
+      line: string;
+    }
+  | {
+      evt: "finish_span";
+      id: number;
+      ts: string;
+    };
+
+class Appc extends React.Component<{}, { events: TraceEvent[] }> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { events: [] };
+  }
+
+  componentDidMount() {
+    const ws = new WebSocket(wsURL);
+    ws.addEventListener("message", evt => {
+      this.setState({ events: [...this.state.events, JSON.parse(evt.data)] });
+    });
+    ws.addEventListener("error", evt => {
+      console.log("error", evt);
+    });
+    ws.addEventListener("close", evt => {
+      console.log("close", evt);
+    });
+    ws.addEventListener("open", evt => {
+      console.log("open", evt);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Messages</h1>
+        <ul>
+          {this.state.events.map((msg, idx) => (
+            <li key={idx.toString()}>
+              <pre>{JSON.stringify(msg)}</pre>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Appc;
+
+// TODO(vilterp): figure out how to use hooks for this
+// function App() {
+//   const [messages, setMessages] = useState<TraceEvent[]>([]);
+//
+//   useEffect(() => {
+//     console.log("connecting");
+//     const ws = new WebSocket(wsURL);
+//     ws.addEventListener("message", evt => {
+//       setMessages([...messages, evt.data]);
+//     });
+//     ws.addEventListener("error", evt => {
+//       console.log("error", evt);
+//     });
+//     ws.addEventListener("close", evt => {
+//       console.log("close", evt);
+//     });
+//     ws.addEventListener("open", evt => {
+//       console.log("open", evt);
+//     });
+//   });
+//
+//   return (
+//     <div>
+//       <h1>Messages</h1>
+//       <ul>
+//         {messages.map((idx, msg) => (
+//           <li key={idx.toString()}>
+//             <pre>{JSON.stringify(msg)}</pre>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -3,24 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const wsAddr =
-  process.env.NODE_ENV === "development"
-    ? "localhost:8888"
-    : window.location.host;
-const wsURL = `ws://${wsAddr}/ws`;
-
-const ws = new WebSocket(wsURL);
-ws.addEventListener("message", evt => {
-  console.log("message", evt);
-});
-ws.addEventListener("error", evt => {
-  console.log("error", evt);
-});
-ws.addEventListener("close", evt => {
-  console.log("close", evt);
-});
-ws.addEventListener("open", evt => {
-  console.log("open", evt);
-});
-
 ReactDOM.render(<App />, document.getElementById("root"));
