eebdf437de2d3fc735751f456e370de3b58ca3b1
Pete Vilter <pete.vilter@gmail.com>
2019-11-30 15:21:16 -0500

    adjustible-width sidebar

--- trace-ui/src/util/PanelLayout.tsx
+++ trace-ui/src/util/PanelLayout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./PanelLayout.css";
 
@@ -7,10 +7,35 @@ function PanelLayout(props: {
   sidebar: React.ReactNode;
   titleArea: React.ReactNode;
 }) {
+  const [sidebarWidthPx, setSidebarWidthPx] = useState(300);
+  const [dragging, setDragging] = useState(false);
+
   return (
-    <div className="panel-layout">
+    <div
+      className="panel-layout"
+      style={{
+        gridTemplateColumns: `auto 5px ${sidebarWidthPx}px`,
+        userSelect: dragging ? "none" : "inherit",
+        cursor: dragging ? "grabbing" : "default"
+      }}
+      onMouseMove={evt => {
+        if (dragging) {
+          setSidebarWidthPx(window.innerWidth - evt.clientX);
+        }
+      }}
+      onMouseUp={() => {
+        if (dragging) {
+          setDragging(false);
+        }
+      }}
+    >
       <div className="panel-layout__title-area">{props.titleArea}</div>
       <div className="panel-layout__main-content">{props.mainContent}</div>
+      <div
+        className="panel-layout__splitter"
+        onMouseDown={() => setDragging(true)}
+        style={{ cursor: dragging ? "inherit" : "grab" }}
+      />
       <div className="panel-layout__sidebar">{props.sidebar}</div>
     </div>
   );
